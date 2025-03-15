04faf6ef7fd58a4cec1d4acfd57e522a67ca5483
Jay Guruswamy <Jayaraj.guruswamy@gmail.com>
2025-03-14 20:43:51 -0400

    first commit

---
+++ WordVaultTests/WordVaultTests.swift
@@ -0,0 +1,10 @@
+import Testing
+@testable import WordVault
+
+struct WordVaultTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
